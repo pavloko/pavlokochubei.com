df2d38363c32b843131f6a1c9f6cfde0bea6cbfd
pavloko <pavlo.kochubei@hotmail.com>
2017-10-29 21:28:38 +0200

    Initial gatsby setup

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people. This is Pavlo Kochubei's website.</h1>
+    <p>Let's build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
