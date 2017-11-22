22503a4a36d2d47ff2386be19dbdd7624691bf9a
pavloko <pavlo.kochubei@hotmail.com>
2017-11-22 22:34:32 +0200

    Refactor hotjar plugin harcoded values to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,16 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-next',
-    'gatsby-plugin-hotjar',
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-hotjar',
+      options: {
+        sv: 6,
+        id: 700184,
+        host: 'pavlokochubei.com'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-57412185-4',
         anonymize: true
--- plugins/gatsby-plugin-hotjar/gatsby-ssr.js
+++ plugins/gatsby-plugin-hotjar/gatsby-ssr.js
@@ -2,15 +2,16 @@ import React from "react"
 
 exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
   if (process.env.NODE_ENV === `production`) {
+    const {id, sv, host} = pluginOptions
     return setPostBodyComponents([
       <script
         key={`gatsby-plugin-hotjar`}
         dangerouslySetInnerHTML={{
           __html: `
-          if (location.host === 'pavlokochubei.com') {
+          if (location.host === '${host}') {
                 (function(h,o,t,j,a,r){
                     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:700184,hjsv:6};
+                    h._hjSettings={hjid:${id},hjsv:${sv}};
                     a=o.getElementsByTagName('head')[0];
                     r=o.createElement('script');r.async=1;
                     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
