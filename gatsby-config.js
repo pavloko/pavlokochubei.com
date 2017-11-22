7177d9c7c5549c7cea574c644dc36cfacea914d0
pavloko <pavlo.kochubei@hotmail.com>
2017-11-22 22:07:30 +0200

    Add hotjar plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-next',
+    'gatsby-plugin-hotjar',
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
