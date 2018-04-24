f58524a22b728f25adfc541b20973bffa1fbfde4
pavloko <pavlo.kochubei@hotmail.com>
2018-04-24 23:20:25 +0300

    Add yandex-metrics

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,15 @@ module.exports = {
         trackingId: 'UA-57412185-4',
         anonymize: true
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-yandex-metrika`,
+      options: {
+        trackingId: '37769640',
+        webvisor: true,
+        trackHash: true,
+        version: 2,
+      },
+    },
   ],
 }
