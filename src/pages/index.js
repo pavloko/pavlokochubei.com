75ecb8bae43d81224b4699f53adb4774fadd82fd
pavloko <pavlo.kochubei@gmail.com>
2018-05-16 17:14:44 +0300

    Engly --> TellProof

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ class IndexPage extends React.Component {
         <h1>I am Pavlo Kochubei.</h1>
         <p>My mission is building elegant, intuitive and intelligent software.</p>
         <p>I love education, design, code, and data.</p>
-        <p>I currently work on <a href="https://www.engly.co/">Engly.</a></p>
+        <p>I currently work on <a href="https://www.tellproof.com/">TellProof.</a></p>
         <SocialLinks />
       </main>
     )
